931484a5d25bb9fa817d1ee8ae586df466a34578
Trevor Xiao <txiao@redhat.com>
2019-09-09 12:49:55 -0400

    Add slf4j logging

--- code/custom-loader/src/main/java/com/rhc/jdg/util/XiaoCacheLoader.java
+++ code/custom-loader/src/main/java/com/rhc/jdg/util/XiaoCacheLoader.java
@@ -1,43 +1,48 @@
 package com.rhc.jdg.util;
 
+import org.infinispan.commons.CacheException;
 import org.infinispan.commons.configuration.ConfiguredBy;
 import org.infinispan.commons.persistence.Store;
 import org.infinispan.marshall.core.MarshalledEntry;
 import org.infinispan.persistence.spi.CacheLoader;
 import org.infinispan.persistence.spi.InitializationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Store
 @ConfiguredBy(XiaoStoreConfiguration.class)
 public class XiaoCacheLoader<K,V> implements CacheLoader<K, V> {
+	
+	private static Logger logger = LoggerFactory.getLogger(XiaoCacheLoader.class);
 
 	public void start() {
 		// TODO Auto-generated method stub
-		System.out.println("======STARTING");
+		logger.info("======STARTING");
 		
 	}
 
 	public void stop() {
 		// TODO Auto-generated method stub
-		System.out.println("======STOPPING");
+		logger.info("======STOPPING");
 		
 	}
 
 	public boolean contains(Object arg0) {
 		// TODO Auto-generated method stub
-		System.out.println("======CHECKING CONTAINS");
+		logger.info("======CHECKING CONTAINS");
 		return false;
 	}
 
 	public void init(InitializationContext arg0) {
 		// TODO Auto-generated method stub
-		System.out.println("======INITING");
+		logger.info("======INITING");
 		
 	}
 
 	public MarshalledEntry<K, V> load(Object arg0) {
 		// TODO Auto-generated method stub
-		System.out.println("======LOADING");
-		return null;
+		logger.info("======LOADING");
+		throw new CacheException("Unable to load object");
 	}
 
 }
